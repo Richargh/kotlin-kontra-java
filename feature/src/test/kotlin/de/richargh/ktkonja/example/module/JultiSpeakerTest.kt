7eeb51335426a8852929c6645fcdba28be58db8a
Richard Gross <richard.gross@maibornwolff.de>
2025-03-31 11:22:18 +0200

    .r: optimize imports

--- feature/src/test/kotlin/de/richargh/ktkonja/example/module/JultiSpeakerTest.kt
+++ feature/src/test/kotlin/de/richargh/ktkonja/example/module/JultiSpeakerTest.kt
@@ -1,6 +1,5 @@
 package de.richargh.ktkonja.example.module
 
-import de.richargh.ktkonja.example.module.JultiSpeaker
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
--- feature/src/test/kotlin/de/richargh/ktkonja/example/module/KultiSpeakerTest.kt
+++ feature/src/test/kotlin/de/richargh/ktkonja/example/module/KultiSpeakerTest.kt
@@ -1,6 +1,5 @@
 package de.richargh.ktkonja.example.module
 
-import de.richargh.ktkonja.example.module.KultiSpeaker
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
