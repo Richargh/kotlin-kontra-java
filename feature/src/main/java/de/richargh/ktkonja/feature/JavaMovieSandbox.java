30d477901a1d88a0cdb6fdf3b8eb5d5369e6422a
Richard Gross <richard.gross@maibornwolff.de>
2025-12-10 21:32:04 +0100

    r: add prepared statement string template import and another for the builder, in case we need it

--- feature/src/main/java/de/richargh/ktkonja/feature/JavaMovieSandbox.java
+++ feature/src/main/java/de/richargh/ktkonja/feature/JavaMovieSandbox.java
@@ -1,5 +1,8 @@
 package de.richargh.ktkonja.feature;
 
+import java.util.function.Consumer;
+import static de.richargh.ktkonja.example.stringtemplates.QueryBuilder.*;
+
 public class JavaMovieSandbox {
     public static void main(String[] args) {
         System.out.println("Java Movie Sandbox");
